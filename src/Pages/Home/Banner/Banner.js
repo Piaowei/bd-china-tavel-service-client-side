76b884726195bdacbe2b756b36223d4793f7f238
Piaowei <msptonmoy@gmail.com>
2021-10-30 19:51:34 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,48 +1,44 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import banner1 from '../../../images/banner/banner1.jpg';
-import banner2 from '../../../images/banner/banner2.jpg';
-import banner3 from '../../../images/banner/banner3.jpg';
+import banner1 from '../../../images/slider/hp1-slider-img1.jpg'
+import banner2 from '../../../images/slider/hp1-slider-img2.jpg'
+import banner3 from '../../../images/slider/hp1-slider-img3.jpg'
 
 const Banner = () => {
     return (
         <>
             <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner1}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner2}
-                        alt="Second slide"
-                    />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
                 <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner3}
-                        alt="Third slide"
-                    />
+                    <div class="card bg-dark text-white  border-0 rounded-0">
+                        <img src={banner1} className="card-img border-0" alt="..." />
+                        <div class="card-img-overlay d-flex justify-content-center align-items-center  border-0">
+                            <h1 className="card-title text-center ">Card title</h1>
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
+                        </div>
+                    </div>
                 </Carousel.Item>
+
+                <Carousel.Item>
+                    <div class="card bg-dark text-white  border-0 rounded-0">
+                        <img src={banner2} className="card-img border-0" alt="..." />
+                        <div class="card-img-overlay d-flex justify-content-center align-items-center  border-0">
+                            <h1 className="card-title text-center ">Card title</h1>
+
+                        </div>
+                    </div>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <div class="card bg-dark text-white  border-0 rounded-0">
+                        <img src={banner3} className="card-img border-0" alt="..." />
+                        <div class="card-img-overlay d-flex justify-content-center align-items-center  border-0">
+                            <h1 className="card-title text-center ">Card title</h1>
+
+                        </div>
+                    </div>
+                </Carousel.Item>
+
             </Carousel>
         </>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,32 +1,59 @@
 import React from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
-import { HashLink } from 'react-router-hash-link';
+import "./HeaderItem.css"
+
+
+
 
 const Header = () => {
     const { user, logOut } = useAuth();
     return (
-        <>
-            <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg" >
-                <Container>
-                    <Navbar.Brand href="#home">Genius</Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
-                        <Nav.Link as={HashLink} to="/manageAllOrders">Manage Orders</Nav.Link>
-                        <Nav.Link as={HashLink} to="/myOrders">My Orders</Nav.Link>
-                        <Nav.Link as={HashLink} to="/addservice">Add a New Service</Nav.Link>
-                        {user?.email ?
-                            <Button onClick={logOut} variant="light">Logout</Button> :
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>}
-                        <Navbar.Text>
-                            Signed in as: <a href="#login">{user?.displayName}</a>
-                        </Navbar.Text>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <div className="sticky-top">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light nav-color ">
+                <div className="container-fluid nav-color">
+                    <a className="navbar-brand " href="#">
+                        {/* <img src={img} alt="" width="30" height="24" className="d-inline-block align-text-top" /> */}
+                        <span className="ps-3 fw-bold " >Bd-China <br /> Travel Service</span>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        {/* <a className="navbar-brand" href="#">Sunrise Eye Care</a> */}
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item hover-link ">
+                                <NavLink className="nav-link active fw-bolder hover-link text-color hover-link text-white" aria-current="page" to="/home">Home</NavLink>
+                            </li>
+                            <li className="nav-item hover-link">
+                                <NavLink className="nav-link active  fw-bolder hover-link text-color hover-link text-white" aria-current="page" to="/manageAllOrders">Manage Orders</NavLink>
+                            </li>
+                            <li className="nav-item hover-link">
+                                <NavLink className="nav-link active  fw-bolder hover-link text-color  hover-link text-white" aria-current="page" to="/myOrders"> My Orders</NavLink>
+                            </li>
+                            <li className="nav-item hover-link">
+                                <NavLink className="nav-link active fw-bolder hover-link text-color hover-link text-white" aria-current="page" to="/addservice">Add a New Service</NavLink>
+                            </li>
+
+                        </ul>
+                        <form className="d-flex justify-content-center pt-3">
+
+                            {user?.email ?
+                                // <Nav.Link as={Link} to="/login">Login</Nav.Link> :
+
+                                <p className="nav-link active fs-6 fw-bolder hover-link text-warning " onClick={logOut}>Logout</p> :
+
+                                <NavLink className="nav-link active fs-6 fw-bolder hover-link " aria-current="page" to="/login">Login</NavLink>}
+
+                            <p className="nav-link active fs-9 fw-bolder hover-link">
+                                <small>Signed in as: {user?.displayName}</small>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
     );
 };
 
